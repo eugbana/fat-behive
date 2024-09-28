156b4ff1fc56a578d339b29c9f3575d504e8cabf
Dominic Ugbana <dugbana@gmail.com>
2024-09-28 23:03:14 +0100

    code debugging test

---
+++ filter.php
@@ -0,0 +1,14 @@
+<?php
+function modify_books_archive_query( $query ) { 
+    if ( is_post_type_archive( 'book' ) && !is_admin() && $query->is_main_query() ) {
+        $tax_query = array(
+            array(
+                'taxonomy' => 'genre',
+                'field'    => 'slug',
+                'terms'    => 'science-fiction',
+            ),
+        );
+        $query->set( 'tax_query', $tax_query ); 
+    } 
+}
+add_action( 'pre_get_posts', 'modify_books_archive_query' );
